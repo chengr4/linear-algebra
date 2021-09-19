15e9640ae67203535e5781adcb2e93bb98db4c8c
Eddie Cheng <fcheng@th-wildau.de>
2021-09-19 22:50:25 +0800

    update 2D vector

--- TwoDimensionalVector.ts
+++ TwoDimensionalVector.ts
@@ -1,3 +1,15 @@
 class TwoDimensionalVector {
-  
+  private v: number[][];
+
+  constructor(value: number[][]) {
+    this.v = value;
+  }
+
+  get value() {
+    return this.v;
+  }
+
+  set value(newValue: number[][]) {
+    this.v = newValue;
+  }
 }
