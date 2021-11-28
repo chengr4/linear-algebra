ab6c2dd0b6080e47ce66b7c5e9a27531ea10267c
Eddie Cheng <schaffenneu47@gmail.com>
2021-11-29 00:11:40 +0800

    finish until 2B 47:00

--- solving-linear-equations/main.go
+++ solving-linear-equations/main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
-	"linear.algebra/pkg/bruteForce"
+	"gonum.org/v1/gonum/mat"
+	"log"
 )
 
-// Inverse Matrix (反矩陣)
+func matPrint(X mat.Matrix) {
+	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%v\n", fa)
+}
 
 func main() {
-	fmt.Println(bruteForce.Test())
+	// Elimation matrix
+	E21 := mat.NewDense(3, 3, []float64{
+		1, 0, 0,
+		-4, 1, 0,
+		0, 0, 1,
+	})
+	matPrint(E21)
+	// Compute the inverse of A.
+	var E21Inv mat.Dense
+	err := E21Inv.Inverse(E21)
+	if err != nil {
+		log.Fatalf("A is not invertible: %v", err)
+	}
+	matPrint(&E21Inv)
+	/*
+		⎡1  0  -0⎤
+		⎢4  1  -0⎥
+		⎣0  0   1⎦
+	*/
 }
